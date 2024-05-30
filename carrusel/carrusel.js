7e68a2c90e98395141a1384a4d269d757afc093c
Diego Sebastian Rodriguez Recalde <dsrrdiego@hotmail.com>
2024-05-30 12:48:46 -0300

    version de muestra

--- carrusel/carrusel.js
+++ carrusel/carrusel.js
@@ -1,110 +1,57 @@
 class Carrusel {
-    constructor(padre, cards) {
-        this.carrusel = document.createElement('div');
-        this.carrusel.classList.add('flex');
+    class Carrusel {
+        constructor(padre, cards) {
+            this.carrusel = document.createElement('div');
+            this.carrusel.classList.add('flex');
 
-        this.contenedorInterno = document.createElement('div');
-        this.contenedorInterno.classList.add('carruselContenedorInterno');
+            this.contenedorInterno = document.createElement('div');
+            this.contenedorInterno.classList.add('carruselContenedorInterno');
 
-        this.btnMenos = document.createElement('button');
-        this.btnMenos.innerHTML = "<";
-        this.btnMenos.onclick = () => this.click(-1);
-        this.btnMenos.classList.add('carruselBtn');
-
-
-        this.slide = document.createElement('div');
-        this.slide.classList.add('flex');
-        this.slide.classList.add('carruselSlide');
-        this.slide.innerHTML = cards;
+            this.btnMenos = document.createElement('button');
+            this.btnMenos.innerHTML = "<";
+            this.btnMenos.onclick = () => this.click(-1);
+            this.btnMenos.classList.add('carruselBtn');
 
 
-        this.btnMas = document.createElement('button');
-        this.btnMas.innerHTML = ">";
-        this.btnMas.onclick = () => this.click(1);
-        this.btnMas.classList.add('carruselBtn');
-        this.btnMas.classList.add('carruselBtnMas');
+            this.slide = document.createElement('div');
+            this.slide.classList.add('flex');
+            this.slide.classList.add('carruselSlide');
+
+            for (const card of cards) {
+                this.slide.appendChild(card);
+            }
 
 
-        this.carrusel.appendChild(this.btnMenos);
-        this.contenedorInterno.appendChild(this.slide);
-        this.carrusel.appendChild(this.contenedorInterno);
-        this.carrusel.appendChild(this.btnMas);
-        padre.innerHTML = "";
-        padre.appendChild(this.carrusel);
-        this.ocultarBotones();
-    }
+            this.btnMas = document.createElement('button');
+            this.btnMas.innerHTML = ">";
+            this.btnMas.onclick = () => this.click(1);
+            this.btnMas.classList.add('carruselBtn');
+            this.btnMas.classList.add('carruselBtnMas');
 
-    click(sentido) {
-        let leftActual = this.slide.scrollLeft;
-        let suma = leftActual + this.slide.clientWidth * 0.8 * sentido;
-        if (leftActual + this.slide.clientWidth >= this.slide.scrollWidth) suma = 0;
-        this.slide.scrollLeft = suma;
-        this.ocultarBotones();
-    }
 
-    ocultarBotones() {
-        this.btnMenos.classList.remove('carruselInvisible');
-        this.btnMas.classList.remove('carruselInvisible');
-        if (this.slide.scrollWidth == this.slide.clientWidth) {
+            this.carrusel.appendChild(this.btnMenos);
+            this.contenedorInterno.appendChild(this.slide);
+            this.carrusel.appendChild(this.contenedorInterno);
+            this.carrusel.appendChild(this.btnMas);
+            padre.innerHTML = "";
+            padre.appendChild(this.carrusel);
+            if (cards.length < 2) this.ocultarBotones();
+
+        }
+
+        click(sentido) {
+            let leftActual = this.slide.scrollLeft;
+            let suma = leftActual + this.slide.clientWidth * 0.8 * sentido;
+            if (leftActual + this.slide.clientWidth >= this.slide.scrollWidth) suma = 0;
+            this.slide.scrollLeft = suma;
+        }
+
+        ocultarBotones() {
+
+            this.btnMenos.classList.remove('carruselInvisible');
+            this.btnMas.classList.remove('carruselInvisible');
             this.btnMenos.classList.add('carruselInvisible');
             this.btnMas.classList.add('carruselInvisible')
         }
-    }
-
-}
-class Carrusel2 {
-    constructor(padre, cards) {
-        this.carrusel = document.createElement('div');
-        this.carrusel.classList.add('flex');
-
-        this.contenedorInterno = document.createElement('div');
-        this.contenedorInterno.classList.add('carruselContenedorInterno');
-
-        this.btnMenos = document.createElement('button');
-        this.btnMenos.innerHTML = "<";
-        this.btnMenos.onclick = () => this.click(-1);
-        this.btnMenos.classList.add('carruselBtn');
-
-
-        this.slide = document.createElement('div');
-        this.slide.classList.add('flex');
-        this.slide.classList.add('carruselSlide');
-
-        for (const card of cards) {
-            this.slide.appendChild(card);
-        }
-
-
-        this.btnMas = document.createElement('button');
-        this.btnMas.innerHTML = ">";
-        this.btnMas.onclick = () => this.click(1);
-        this.btnMas.classList.add('carruselBtn');
-        this.btnMas.classList.add('carruselBtnMas');
-
-
-        this.carrusel.appendChild(this.btnMenos);
-        this.contenedorInterno.appendChild(this.slide);
-        this.carrusel.appendChild(this.contenedorInterno);
-        this.carrusel.appendChild(this.btnMas);
-        padre.innerHTML = "";
-        padre.appendChild(this.carrusel);
-        if (cards.length < 2) this.ocultarBotones();
 
     }
-
-    click(sentido) {
-        let leftActual = this.slide.scrollLeft;
-        let suma = leftActual + this.slide.clientWidth * 0.8 * sentido;
-        if (leftActual + this.slide.clientWidth >= this.slide.scrollWidth) suma = 0;
-        this.slide.scrollLeft = suma;
-    }
-
-    ocultarBotones() {
-
-        this.btnMenos.classList.remove('carruselInvisible');
-        this.btnMas.classList.remove('carruselInvisible');
-        this.btnMenos.classList.add('carruselInvisible');
-        this.btnMas.classList.add('carruselInvisible')
-    }
-
-}
